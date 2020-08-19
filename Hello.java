204a9bc3a89d4b897ab0167fb05cd7b7522efb6f
a-welk <69910428+a-welk@users.noreply.github.com>
2020-08-19 11:00:04 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/************************************************************
+* Hello.java
+*************************************************************
+* Alex Welk
+* 8/19/20
+* CSMC 255
+*************************************************************/
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Alex Welk");
+		System.out.println("hello.java");
+		System.out.println("give us the very basics of java");
+		System.out.println("8/19/20");
+		System.out.println("CMSC255");
+		System.out.println();
+	}
+}
